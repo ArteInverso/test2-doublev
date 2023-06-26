517b23255ae1657865f9fd50392205ad8912677d
ArteInverso <info@arteinverso.co>
2023-06-26 12:18:46 -0500

    fix

--- Program.cs
+++ Program.cs
@@ -1,4 +1,3 @@
-// Importar los espacios de nombres necesarios
 using System;
 using NBitcoin;
 using Nethereum.Hex.HexConvertors.Extensions;
@@ -6,13 +5,13 @@ using Nethereum.HdWallet;
 using Nethereum.Signer;
 using Nethereum.Util;
 
-namespace Generador
+namespace TuNamespace
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // Generar las palabras del mnemónico y llamar a la función para crear la wallet Ethereum
+            // Generar las palabras mnemotécnicas y crear una wallet de Ethereum
             var mnemonicWords = GeneratePrivateKeyAndMnemonic();
             CreateEthereumWallet(mnemonicWords);
         }
@@ -26,8 +25,8 @@ namespace Generador
             Console.WriteLine("Llave privada generada:");
             Console.WriteLine(privateKeyHex);
 
-            // Generar conjunto de palabras para recrear la llave privada
-            var mnemonic = new Mnemonic(Wordlist.English, WordCount.Twelve);
+            // Generar conjunto de palabras mnemotécnicas para recrear la llave privada
+            var mnemonic = new Mnemonic(Wordlist.English);
             var mnemonicWords = mnemonic.ToString();
 
             Console.WriteLine("\nConjunto de palabras generadas:");
@@ -38,21 +37,16 @@ namespace Generador
 
         static void CreateEthereumWallet(string mnemonicWords)
         {
-            // Crear una instancia de Mnemonic utilizando las palabras generadas
-            var mnemonic = new Mnemonic(mnemonicWords);
-
-            // Derivar la semilla a partir del mnemónico
+            // Crear una wallet de Ethereum a partir de las palabras mnemotécnicas
+            var mnemonic = new Mnemonic(mnemonicWords, Wordlist.English);
             var seed = mnemonic.DeriveSeed();
 
-            // Crear una wallet Ethereum utilizando la semilla y una contraseña
-            var hdWallet = new Wallet(seed, "password");
-
-            // Obtener la cuenta Ethereum de la wallet en el índice 0
+            // Derivar una cuenta de Ethereum de la wallet
+            var hdWallet = new Wallet(seed, "m/44'/60'/0'/0/0");
             var account = hdWallet.GetAccount(0);
 
-            // Obtener la dirección de la cuenta Ethereum
+            // Obtener la dirección de la cuenta en minúsculas
             var address = account.Address.ToLower();
-
             Console.WriteLine("\nDirección de la wallet Ethereum:");
             Console.WriteLine(address);
         }
